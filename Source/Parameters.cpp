680edcfaed28b18878756418753222ef396de4ba
francobach47 <francobach47@gmail.com>
2025-02-26 20:49:40 -0300

    ms from string in delay param

--- Source/Parameters.cpp
+++ Source/Parameters.cpp
@@ -24,6 +24,17 @@ static juce::String stringFromMilliseconds(float value, int)
 	}
 }
 
+static float millisecondsFromString(const juce::String& text)
+{
+	float value = text.getFloatValue();
+	if (!text.endsWithIgnoreCase("ms")) {
+		if (text.endsWithIgnoreCase("s") || value < Parameters::minDelayTime) {
+			return value * 1000.0f;
+		}
+	}
+	return value;
+}
+
 static juce::String stringFromDecibels(float value, int)
 {
 	return juce::String(value, 1) + " dB";
@@ -58,7 +69,7 @@ juce::AudioProcessorValueTreeState::ParameterLayout Parameters::createParameterL
 		"Delay Time",
 		juce::NormalisableRange<float> { minDelayTime, maxDelayTime, 0.001f, 0.25f },
 		100.0f,
-		juce::AudioParameterFloatAttributes().withStringFromValueFunction(stringFromMilliseconds)
+		juce::AudioParameterFloatAttributes().withStringFromValueFunction(stringFromMilliseconds).withValueFromStringFunction(millisecondsFromString)
 	));
 
 	layout.add(std::make_unique<juce::AudioParameterFloat>(
