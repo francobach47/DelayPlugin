73e8c3bf77428c2f944d6da7e207dff168ce5481
francobach47 <francobach47@gmail.com>
2025-02-26 21:58:15 -0300

    gain knob UI slider

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,11 +1,3 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
@@ -13,9 +5,18 @@
 DelayAudioProcessorEditor::DelayAudioProcessorEditor (DelayAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    slider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
+    slider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 70, 16);
+    slider.setBounds(0, 0, 70, 86);
+    addAndMakeVisible(slider);
+
+    label.setText("Gain", juce::NotificationType::dontSendNotification);
+    label.setJustificationType(juce::Justification::horizontallyCentred);
+    label.setBorderSize(juce::BorderSize<int>{ 0, 0, 2, 0});
+    label.attachToComponent(&slider, false);
+    addAndMakeVisible(label);
+    
+    setSize (500, 330);
 }
 
 DelayAudioProcessorEditor::~DelayAudioProcessorEditor()
@@ -25,16 +26,10 @@ DelayAudioProcessorEditor::~DelayAudioProcessorEditor()
 //==============================================================================
 void DelayAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("__ColoDSP__", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll (juce::Colours::darkgrey);
 }
 
 void DelayAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    slider.setTopLeftPosition(215, 120);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,16 +1,8 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #pragma once
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
-
+#include "Parameters.h"
 //==============================================================================
 /**
 */
@@ -25,9 +17,14 @@ public:
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
     DelayAudioProcessor& audioProcessor;
+
+    juce::Slider slider;
+    juce::Label label;
+
+    juce::AudioProcessorValueTreeState::SliderAttachment attachment{
+        audioProcessor.apvts, gainParamID.getParamID(), slider
+    };
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DelayAudioProcessorEditor)
 };
