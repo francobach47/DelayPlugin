46772f226ec16f1f116eb393d4cf1c7c0f012b15
francobach47 <francobach47@gmail.com>
2025-03-04 13:24:09 -0300

    level meter bars

--- Source/LevelMeter.h
+++ Source/LevelMeter.h
@@ -15,8 +15,29 @@ public:
 private:
     void timerCallback() override;
 
+    int positionForLevel(float dbLevel) const noexcept
+    {
+        return int(std::round(juce::jmap(dbLevel, maxdB, mindB, maxPos, minPos)));
+    }
+
+    void drawLevel(juce::Graphics& g, float level, int x, int width);
+
     std::atomic<float>& measurementL;
     std::atomic<float>& measurementR;
+
+    static constexpr float maxdB = 6.0f;
+    static constexpr float mindB = -60.0f;
+    static constexpr float stepdB = 6.0f;
+
+    float maxPos = 0.0f;
+    float minPos = 0.0f;
+
+    static constexpr float clampdB = -120.0f;
+    static constexpr float clampLevel = 0.000001f; // - 120 dB
+
+    float dbLevelL;
+    float dbLevelR;
+
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LevelMeter)
 };
